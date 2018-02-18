c69d1197f469a7ee87c8a98e24793e5ce03fb50d
Roy Segall <roy.segall@gizra.com>
2018-02-18 16:14:14 +0200

    #9: When the login register it to a log file.

--- server/src/Controller/Login.php
+++ server/src/Controller/Login.php
@@ -3,8 +3,10 @@
 namespace Social\Controller;
 
 use Social\ControllerBase;
+use Social\Entity\Logs;
 use Social\Entity\User;
 use Social\EntityBase;
+use Symfony\Component\Filesystem\Filesystem;
 
 class Login extends ControllerBase {
 
@@ -33,7 +35,9 @@ class Login extends ControllerBase {
       ->filter((array) $data)
       ->run($user->getConnection());
 
-    if (!$results) {
+    if (!$results->toArray()) {
+      // Writing to the log.
+      $this->logFailedLogin($data->username);
       $this->badRequest('No username was found. Try again later.');
     }
 
@@ -41,6 +45,39 @@ class Login extends ControllerBase {
     // that.
     // todo: create access token.
     return EntityBase::processCursor($results);
+  }
+
+  /**
+   * Log how many times the user failed with login from the same system.
+   *
+   * @param string $username
+   *   The username.
+   */
+  protected function logFailedLogin($username) {
+    $logs = new Logs();
+    $document = [
+      'username' => $username,
+      'ip' => $this->request->server->get("REMOTE_ADDR"),
+    ];
+
+    $results = $logs
+      ->getTable()
+      ->filter($document)
+      ->run($logs->getConnection());
+
+    if (count($results->toArray()) >= 5) {
+      // Write to the log.
+      $data = [
+        'ip' => $this->request->server->get("REMOTE_ADDR"),
+        'date' => date('m/d/Y i:H'),
+      ];
+      $string = "Failed login occurred at {$data['date']} from the IP {$data['ip']}\n";
+
+      $fs = new Filesystem();
+      $fs->appendToFile('failed_logs.log', $string);
+    }
+
+    $logs->save($document);
   }
 
 }
