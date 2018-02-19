9ad75ef24fe05991af3fde86fab70d97ea928314
RoySegall <roy.segall@gizra.com>
2018-02-19 17:18:50 +0200

    Adding stuff.

--- mini-social/src/Login.js
+++ mini-social/src/Login.js
@@ -1,13 +1,79 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Login.css';
+import * as axios from "axios";
+import settings from './settings';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.errors = "A";
+
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
   render() {
     return (
       <div className="Login">
-          Log in!
+
+        {this.state.errors}
+
+        <div className="form-element username">
+          <label>Username</label>
+          <input
+            id="username"
+            type="text"
+            placeholder="Username"
+            onChange={(event) => this.setState({username: event.target.value})}/>
+        </div>
+
+        <div className="form-element username">
+          <label>Password</label>
+          <input type="password" placeholder="Password"
+                 onChange={(event) => this.setState({password: event.target.value})}/>
+        </div>
+
+        <div className="actions">
+          <button className="push_button blue"
+                  onClick={(event) => this.handleClick(event, this)}>Log me in Scotty
+          </button>
+        </div>
+
       </div>
     );
+  }
+
+  handleClick(event, obj) {
+    // Reset the errors.
+    let errors = [];
+
+    if (this.state.username === "") {
+      errors.push('The username is required');
+    }
+
+    if (this.state.password === "") {
+      errors.push('The password is required');
+    }
+
+    if (errors.length == 0) {
+      axios.
+        post(settings.backend + "/login", {
+          'username': this.state.username,
+          'password': this.state.password,
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    obj.setState({errors: errors.join(", ") + "."});
   }
 }
 
--- mini-social/src/index.js
+++ mini-social/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Login from './Login';
 import Homepage from './HomePage';
 import registerServiceWorker from './registerServiceWorker';
 
-if (false) {
+if (window.localStorage.getItem('uid') == null) {
     ReactDOM.render(<Login />, document.getElementById('root'));
 }
 else {
