f6afb06464f91797daa0d8b37097c56b21977700
Roy Segall <roy.segall@gizra.com>
2018-02-20 21:08:40 +0200

    First work on the page.

--- mini-social/src/HomePage.js
+++ mini-social/src/HomePage.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Login.css';
+import settings from 'settings';
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="Homepage">
-          <button>Show all friends</button>
-          <button>Show birthdays</button>
-          <button>Show potentials friends</button>
-          <button>Show upcoming birthdays</button>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        let friends = ['a', 'b', 'bar'];
+        this.name = <ul>{friends.map((item) => <li>{item}</li>)}</ul>;
+    }
+
+    render() {
+        return (
+            <div className="Homepage">
+                <button>Show all friends</button>
+                <button>Show birthdays</button>
+                <button>Show potentials friends</button>
+                <button>Show upcoming birthdays</button>
+                {this.name}
+            </div>
+        );
+    }
 }
 
 export default Login;
