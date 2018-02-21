6bba25aa2f109f443ce9eb1c62a70001d4683c31
RoySegall <roy.segall@gizra.com>
2018-02-21 18:00:41 +0200

    A lot of improvements.

--- mini-social/src/HomePage.js
+++ mini-social/src/HomePage.js
@@ -2,36 +2,71 @@ import React, {Component} from 'react';
 import './Homepage.css';
 import settings from './settings';
 
-class Login extends Component {
+class Login extends React.Component {
 
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.state= {
-            results: <h2>Hello there! Please do something with the buttons on the left.</h2>
-        };
-    }
+    this.state = {
+      results: <h2>Hello there! Please do something with the buttons on the
+        left.</h2>
+    };
+  }
 
-    showAllFriends() {
-        let friends = ['a', 'b', 'bar'];
-        this.setState({results: <ul>{friends.map((item) => <li>{item}</li>)}</ul>});
-    }
+  showAllFriends() {
+    let friends = ['Roy', 'Roy', 'John'];
+    this.setState({
+      results: <ul>{friends.map((item) => <li>{item}</li>)}</ul>
+    });
+  }
 
-    render() {
-        return (
-            <div className="Homepage">
-                <div className="first">
-                    <button className="actions" onClick={this.showAllFriends}>Show all friends</button>
-                    <button className="actions">Show birthdays</button>
-                    <button className="actions">Show potentials friends</button>
-                    <button className="actions">Show upcoming birthdays</button>
-                </div>
-                <div className="last">
-                    {this.state.results}
-                </div>
-            </div>
-        );
-    }
+  showBirthdays() {
+    let friends = ['Noy'];
+    this.setState({
+      results: <ul>{friends.map((item) => <li>{item}</li>)}</ul>
+    });
+  }
+
+  showPotentialFriends() {
+    let friends = ['Donald'];
+    this.setState({
+      results: <ul>{friends.map((item) => <li>{item}</li>)}</ul>
+    });
+  }
+
+  showUpcomingBirthdays() {
+    let friends = ['Rick'];
+    this.setState({
+      results: <ul>{friends.map((item) => <li>{item}</li>)}</ul>
+    });
+  }
+
+  render() {
+    return (
+      <div className="Homepage">
+        <div className="first">
+          {/*<button className="actions" onClick={this.showAllFriends(this)}>Show all friends</button>*/}
+          <button className="actions" onClick={() => this.showAllFriends()}>Show
+            all friends
+          </button>
+          <button className="actions" onClick={() => this.showBirthdays()}>Show
+            birthdays
+          </button>
+          <button className="actions"
+                  onClick={() => this.showPotentialFriends()}>Show potentials
+            friends
+          </button>
+          <button className="actions"
+                  onClick={() => this.showUpcomingBirthdays()}>Show upcoming
+            birthdays
+          </button>
+        </div>
+        <div className="last">
+          {this.state.results}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- mini-social/src/Login.js
+++ mini-social/src/Login.js
@@ -5,75 +5,81 @@ import settings from './settings';
 
 class Login extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: '',
-            password: '',
-            errors: [],
-        }
+    this.state = {
+      username: '',
+      password: '',
+      errors: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className="Login">
+
+        {this.state.errors}
+
+        <div className="form-element username">
+          <label>Username</label>
+          <input
+            id="username"
+            type="text"
+            placeholder="Username"
+            onChange={(event) => this.setState({username: event.target.value})}/>
+        </div>
+
+        <div className="form-element username">
+          <label>Password</label>
+          <input type="password" placeholder="Password"
+                 onChange={(event) => this.setState({password: event.target.value})}/>
+        </div>
+
+        <div className="actions">
+          <button className="push_button blue"
+                  onClick={() => this.handleClick()}>Log me in Scotty
+          </button>
+        </div>
+
+      </div>
+    );
+  }
+
+  handleClick() {
+    // Reset the errors.
+    let errors = [];
+
+    if (this.state.username === "") {
+      errors.push('The username is required');
     }
 
-    render() {
-        return (
-            <div className="Login">
-
-                {this.state.errors}
-
-                <div className="form-element username">
-                    <label>Username</label>
-                    <input
-                        id="username"
-                        type="text"
-                        placeholder="Username"
-                        onChange={(event) => this.setState({username: event.target.value})}/>
-                </div>
-
-                <div className="form-element username">
-                    <label>Password</label>
-                    <input type="password" placeholder="Password"
-                           onChange={(event) => this.setState({password: event.target.value})}/>
-                </div>
-
-                <div className="actions">
-                    <button className="push_button blue"
-                            onClick={(event) => this.handleClick(event, this)}>Log me in Scotty
-                    </button>
-                </div>
-
-            </div>
-        );
+    if (this.state.password === "") {
+      errors.push('The password is required');
     }
 
-    handleClick(event, obj) {
-        // Reset the errors.
-        let errors = [];
+    if (errors.length === 0) {
+      let self = this;
 
-        if (this.state.username === "") {
-            errors.push('The username is required');
-        }
-
-        if (this.state.password === "") {
-            errors.push('The password is required');
-        }
-
-        if (errors.length == 0) {
-            axios.post(settings.backend + "/login",
-                "username=" + this.state.username + "&password=" + this.state.password)
-                .then(function (response) {
-                    window.localStorage.setItem('uid', response.data.id);
-                    window.location.reload();
-                })
-                .catch(function (error) {
-                    if (error.response !== undefined) {
-                        obj.setState({errors: error.response.data.error});
-                    }
-                });
-        }
-
-        obj.setState({errors: errors.join(", ")});
+      axios.post(settings.backend + "/login",
+        "username=" + this.state.username + "&password=" + this.state.password)
+        .then(function (response) {
+          window.localStorage.setItem('uid', response.data.id);
+          window.location.reload();
+        })
+        .catch(function (error) {
+          if (error.response !== undefined) {
+            self.setState({
+              errors: error.response.data.error
+            });
+          }
+        });
     }
+
+    this.setState({
+      errors: errors.join(", ")
+    });
+  }
 }
 
 export default Login;
